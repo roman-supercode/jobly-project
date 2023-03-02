2fdca84e754f1afdc055c3ba0ed3e4d2028f3573
roman-supercode <roman.supercode@gmail.com>
2023-03-02 21:05:14 +0100

    pageBtnContainer setup

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -2,6 +2,7 @@ import { useAppContext } from "../context/appContext";
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
 import Wrapper from "../assets/wrappers/PageBtnContainer";
 
+
 const PageBtnContainer = () => {
   const { numOfPage, page } = useAppContext();
   return <div>PageBtnContainer</div>;
