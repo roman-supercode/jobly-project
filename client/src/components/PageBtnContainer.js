bbf3a5cf60aef2d22c6359b7b195d0ce3d7af9f3
Roman <roman.supercode@gmail.com>
2023-03-02 20:49:47 +0100

    pageBtnContainer Setup

---
+++ client/src/components/PageBtnContainer.js
@@ -0,0 +1,9 @@
+import { useAppContext } from "../context/appContext";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+import Wrapper from "../assets/wrappers/PageBtnContainer";
+
+const PageBtnContainer = () => {
+  const { numOfPage, page } = useAppContext();
+  return <div>PageBtnContainer</div>;
+};
+export default PageBtnContainer;
