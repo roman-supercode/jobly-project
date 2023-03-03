36aa07feef0bc81426158e821f684fce04580d65
roman-supercode <roman.supercode@gmail.com>
2023-03-03 13:16:11 +0100

    security packages

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,23 @@
 import express from "express";
 const router = express.Router();
 
+// Basic rate-limiting middleware for Express.
+import rateLimiter from "express-rate-limit";
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message:
+    "Too many requests from this IP address, please try again in 15 minutes",
+});
+
 import { register, login, updateUser } from "../controllers/authController.js";
 import authenticateUser from "../middleware/auth.js";
 
 // POST Anfragen - weiterleitung an "register"
-router.route("/register").post(register);
+router.route("/register").post(apiLimiter, register);
 // POST Anfragen - weiterleitung an "login"
-router.route("/login").post(login);
+router.route("/login").post(apiLimiter, login);
 // PATCH Anfrahen - weiterleitung an "updateUser"
 router.route("/updateUser").patch(authenticateUser, updateUser);
 
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from "express";
-// import cors from "cors";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 import authenticateUser from "./middleware/auth.js";
@@ -7,7 +6,17 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 dotenv.config();
 
+// imports for production build
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
 import "express-async-errors";
+
+// security packages
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
 
 //db and authenticaterUser
 import connectDB from "./db/connect.js";
@@ -19,39 +28,48 @@ import jobsRouter from "./routes/jobsRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 9898;
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // middleware
-// app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+
 if (process.env.NODE_ENV !== "production") {
-    app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
 // test Route
 app.get("/", (req, res) => {
-    res.json({ msg: "Welcome!" });
+  res.json({ msg: "Welcome!" });
 });
-// test Route 
+// test Route
 app.get("/api/v1", (req, res) => {
-    res.json({ msg: "API" });
+  res.json({ msg: "API" });
 });
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 // middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URL);
-        app.listen(PORT, () => {
-            console.log("Server ist listening on PORT:", PORT);
-        });
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, () => {
+      console.log("Server ist listening on PORT:", PORT);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
